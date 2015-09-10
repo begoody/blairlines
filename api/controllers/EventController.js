2180b7f447a347dab0b386689b91e077de0ec034
koshelev.d <koshelev.d@delta.ru>
2015-09-10 13:54:44 +1200

    club user and club event

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -11,7 +11,7 @@ module.exports = {
 		
 		var params = req.params.all();
 		params.pilots = req.session.PilotId;
-		if(req.session.ClubId){ params.club = req.session.ClubId; }
+		if(req.session.ClubId){ params.clubs = req.session.ClubId; }
 		//console.log(params);
 		delete(params.id);
 		
