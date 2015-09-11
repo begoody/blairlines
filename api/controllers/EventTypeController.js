296b96d9a2437684640345cdd1643854e84d282a
koshelev.d <koshelev.d@delta.ru>
2015-09-11 14:26:26 +1200

    Live event data K wow

---
+++ api/controllers/EventTypeController.js
@@ -0,0 +1,11 @@
+/**
+ * EventTypeController
+ *
+ * @description :: Server-side logic for managing eventtypes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
